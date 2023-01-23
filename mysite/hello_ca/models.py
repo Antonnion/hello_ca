86a53ebe748645bc71c9579ccce26a765eb24db9
kei <>
2023-01-23 10:27:04 +0000

    bug fix

--- mysite/hello_ca/models.py
+++ mysite/hello_ca/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Employee:
-    def employee(self, employee_name, employee_intro_text, employee_image):
-        self.name = employee_name
-        self.intro = employee_intro_text
-        self.image = employee_image
+    # def __init__(self, employee_name, employee_intro_text, employee_image):
+    def __init__(self, name, intro_text):
+        self.name = name
+        self.intro_text = intro_text
+        # self.image = image
