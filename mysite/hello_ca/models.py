f0c790f29a2b34c6f344a6d41251ff2718b0a0f6
kei <>
2023-01-23 10:01:52 +0000

    employee model追加

--- mysite/hello_ca/models.py
+++ mysite/hello_ca/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Employee:
+    def employee(self, employee_name, employee_intro_text, employee_image):
+        self.name = employee_name
+        self.intro = employee_intro_text
+        self.image = employee_image
