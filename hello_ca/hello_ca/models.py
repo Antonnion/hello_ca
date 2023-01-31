8901f784e6c7a160d2c8a414dad4e21781ae12e2
Masahiro noma <macharo.0316@gmail.com>
2023-01-31 20:58:27 +0000

    refactor

--- hello_ca/hello_ca/models.py
+++ hello_ca/hello_ca/models.py
@@ -6,10 +6,6 @@ class Employee(models.Model):
     name = models.CharField(max_length=50)
     intro_text = models.CharField(max_length=256)
 
-    # def __init__(self, name, intro_text):
-    #     self.name = name
-    #     self.intro_text = intro_text
-
     def __str__(self) -> str:
         return self.name
 
