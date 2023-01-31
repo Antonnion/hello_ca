cc4e141e7075df143936fd29f3e61732a4d6b398
kei <>
2023-01-31 08:57:25 +0000

    Co-authored-by: Marogosteen <marogosteen@users.noreply.github.com>

--- hello_ca/hello_ca/urls.py
+++ hello_ca/hello_ca/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ListView.as_view(), name='index'),
-    path('<int:pk>/', views.detail, name='detail'),
-    path('upload/', views.upload, name='upload')
+    path('detail/<int:pk>/', views.detail, name='detail'),
+    path('upload/', views.upload, name='upload'),
+    # path('detail/<int:pk>', views.ListView.as_view(), name='detail')
 ]
--- hello_ca/hello_ca/views.py
+++ hello_ca/hello_ca/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.views import generic
 from django.http import HttpRequest
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import generic
+from django.views.decorators.http import require_POST
 
-from .models import Employee
 from . import models
-
+from .models import Employee
 
 class ListView(generic.ListView):
     template_name = "hello_ca/index.html"
@@ -15,8 +15,13 @@ class ListView(generic.ListView):
 
 
 def detail(request: HttpRequest, pk: int):
+    if request.method == "POST":
+        employee = get_object_or_404(models.Employee, pk=pk)
+        employee.delete()
+        return redirect("http://127.0.0.1:8000")
     employee = models.Employee.objects.get(pk=pk)
     return render(request, "hello_ca/detail.html", {"employee": employee})
+    
 
 
 def upload(request: HttpRequest):
@@ -25,3 +30,9 @@ def upload(request: HttpRequest):
         model.save()
         return render(request, 'hello_ca/upload.html')
     return render(request, 'hello_ca/upload.html')
+
+# def delete(request: HttpRequest, pk = int):
+#     if request.method == "POST":
+#         employee = get_object_or_404(pk=pk)
+#         employee.delete()
+#     return redirect('hello_ca/index.html')
