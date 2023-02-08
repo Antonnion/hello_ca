5bb9cc1581eec3a81e5f14769e4efc4c9b063cef
Masahiro noma <macharo.0316@gmail.com>
2023-02-08 17:53:30 +0000

    名前で検索できるようにした

--- hello_ca/hello_ca/views.py
+++ hello_ca/hello_ca/views.py
@@ -1,14 +1,28 @@
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views import generic
+from django.db.models import Q
 
 from . import models
 
 
+DISPLAY_EMPLOYEE_COUNT = 8
+
+
 def index(request: HttpRequest):
+    if len(request.GET):
+        search_name = request.GET["search_name"]
+        employees = models.Employee.objects.filter(
+            Q(first_name__icontains=search_name)
+            | Q(last_name__icontains=search_name)
+            | Q(first_name_kana__icontains=search_name)
+            | Q(last_name_kana__icontains=search_name)
+        )[:DISPLAY_EMPLOYEE_COUNT]
+    else:
+        employees = models.Employee.objects.all()[:DISPLAY_EMPLOYEE_COUNT]
+
     # TODO: employeesが0のときの例外処理が欲しい．
     return render(request, "hello_ca/index.html", {
-        "employees": models.Employee.objects.all()[:8],
+        "employees": employees,
         "depertments": models.Depertment.objects.all()
     })
 
